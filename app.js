8461d9a893e557215c8446366eb9f60dc2dbb57b
lainwagoner <89354036+lainwagoner@users.noreply.github.com>
2021-10-08 21:07:32 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// DO NOT EDIT THE ARRAY TO COMPLETE THE ASSIGNMENT
-let products = [
+var products = [
     {
         id:0,
         name:"Apples",
@@ -23,54 +22,110 @@ let products = [
     }
 ];
 
+// DO NOT EDIT THE ARRAY TO COMPLETE THE ASSIGNMENT
+
+
 // Create a div and give it a class of "popUp". Append that div to the body
-var eles=[];
-function popUp(item) {
 
-    const  that = this;
+const box = [];
 
-    this.popUpEle = document.createElement("div");
-    this.popUpEle.classList.add("popUp")
-
-    document.body.appendChild(this.popUpEle);
-}
-
-for(var i=0; i<this.products.length; i++){
-    this.products[i].popUpEle.addEventListener("click", function() {
-        eles.push(new popUp(products[i]));
-
-    })
-
-// If I click on the popUp div, it should hide it
-
-// Create a div for each item in the array above
-function product(name,price,image,color) {
+function fruit(item) {
     const that = this;
+    this.id = item.id;
+    this.name = item.name;
+    this.price = item.price;
+    this.image = item.image;
+    this.color = item.color;
 
-    this.name = name.item;
-    this.price = price.item;
-    this.image = image.item;
-    this.color = color.item;
+    this.ele = document.createElement("div");
+    this.ele.classList.add("product");
+// Create a div for each item in the array above
 
-    this.itemEle = document.createElement("div")
-    this.nameEle = document.createElement("h4")
-    this.priceEle = document.createElement("div")
-    this.imageEle = document.createElement("h4")
-    this.colorEle = document.createElement("div")
-}
+    this.nameEle = document.createElement("h5");
+    this.priceEle = document.createElement("h5");
+    this.imageEle = document.createElement("div");
+    this.colorEle = document.createElement("div");
 
+    this.popUp = document.createElement("div")
+    this.popUp.classList.add("popUp");
 // inside of each div, the prodcut Name and Price should be written as "Name: value" and "Price: $#.##";
 
-    this.nameEle.innerHTML = "Name: "+this.name;
-    this.priceEle.innerHTML = "Price: $"+this.price;
-
+    this.nameEle.innerHTML = "Name: " + item.name;
+    this.priceEle.innerHTML = "Price: $" + item.price;
+    this.imageEle.innerHTML = "<img src = '" +item.image +"'/>";
+    this.colorEle.innerHTML = item.color;
 // inside of each div, the product's image should be on top of the text
+    this.ele.appendChild(this.imageEle);
+    this.ele.appendChild(this.nameEle);
+    this.ele.appendChild(this.priceEle);
 
-// when hovering over a product, its border color should change to the "color" of that product. Once you hover off, it should return to black
-    this.imageEle.addEventListener("mouseover", function() {
-     that.imageEle.style.borderColor = color
-    })
-    document.body.appendChild(this.imageEle);
+
+// If I click on the popUp div, it should hide it
+    document.body.appendChild(this.popUp)
+    this.popUp.style.display = "none";
 // When Clicking on a product, it should display the popUp Menu over all the content
 
+    this.ele.addEventListener("click", function() {
+        if (item.id == 0) {
+            that.popUp.appendChild(that.imageEle);
+
+            that.popUp.appendChild(that.nameEle);
+            that.popUp.appendChild(that.priceEle);
+            that.popUp.style.display = "flex";
+        } else if (item.id == 1) {
+            that.popUp.appendChild(that.imageEle);
+
+            that.popUp.appendChild(that.nameEle);
+            that.popUp.appendChild(that.priceEle);
+            that.popUp.style.display = "flex";
+        } else if (item.id == 2) {
+            that.popUp.appendChild(that.imageEle);
+
+            that.popUp.appendChild(that.nameEle);
+            that.popUp.appendChild(that.priceEle);
+            that.popUp.style.display = "flex";
+
+        }
+
+
+    });
 // When clicking on the paticular product, that products image, name, and price should be displayed inside the popUp div just like it does in its own product div. No other products information shold be shown.
+
+    this.popUp.addEventListener("click", function() {
+
+        if (item.id == 0) {
+            that.popUp.style.display = "none";
+            that.popUp.appendChild(that.nameEle);
+            that.popUp.appendChild(that.imageEle);
+            that.popUp.appendChild(that.priceEle);
+
+        } else if (item.id == 1) {
+            that.popUp.style.display = "none";
+            that.popUp.appendChild(that.nameEle);
+            that.popUp.appendChild(that.imageEle);
+            that.popUp.appendChild(that.priceEle);
+
+        } else if (item.id == 2) {
+            that.popUp.style.display = "none";
+            that.popUp.appendChild(that.nameEle);
+            that.popUp.appendChild(that.imageEle);
+            that.popUp.appendChild(that.priceEle);
+        }
+    });
+// when hovering over a product, its border color should change to the "color" of that product. Once you hover off, it should return to black
+
+    this.ele.addEventListener("mouseover", function(){
+        that.ele.style.borderColor = item.color
+    });
+
+    this.ele.addEventListener("mouseout", function(){
+        that.ele.style.borderColor = ""
+    });
+
+
+
+    document.body.appendChild(this.ele);
+}
+for(var i=0; i <products.length; i++){
+    box.push(new fruit(products[i]));
+};
